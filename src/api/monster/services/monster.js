6a086eb96a45cc752824c00d41ea2957babf0e30
Spencer Haugh <spencer.haugh@gmail.com>
2023-03-04 18:14:34 -0500

    add endpoints

---
+++ src/api/monster/services/monster.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * monster service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::monster.monster');
