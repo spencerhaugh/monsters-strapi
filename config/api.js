29ed9417c62c42e57d2a4cefd06c9adcaa095ac1
Spencer Haugh <spencer.haugh@gmail.com>
2023-03-04 17:45:39 -0500

    config

--- config/api.js
+++ config/api.js
@@ -1,7 +1,7 @@
 module.exports = {
   rest: {
-    defaultLimit: 25,
-    maxLimit: 100,
+    defaultLimit: 1200,
+    maxLimit: 1200,
     withCount: true,
   },
 };
--- config/plugins.js
+++ config/plugins.js
@@ -18,4 +18,7 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    'import-export-entries': {
+      enabled: true,
+    },
   });
