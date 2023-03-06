73d93b3c3a99d01d9ba4804d8a15950112ba1e25
Spencer Haugh <spencer.haugh@gmail.com>
2023-03-06 18:01:16 -0500

    cloudinary config

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,8 +7,8 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'alex-makes-monsters.up.railway.app'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'alex-makes-monsters.up.railway.app'],
           upgradeInsecureRequests: null,
         },
       },
